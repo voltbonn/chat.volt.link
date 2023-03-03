8972e8189eb0042331c0f49e4261e6fa37d69f24
thomasrosen <thomas.rosen@me.com>
2023-03-03 03:31:20 +0100

    preselect facts to send less data

--- backend/chat.js
+++ backend/chat.js
@@ -5,19 +5,28 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 
-async function ask_the_bot(system_setup, messages, onTokenCallback) {
+async function ask_the_bot(system_setup, messages, onTokenCallback, options = {}) {
   const has_onTokenCallback = typeof onTokenCallback === 'function'
+
+  if (typeof system_setup === 'string') {
+    system_setup = [
+      { role: 'assistant', content: system_setup },
+    ]
+  } else {
+    system_setup = []
+  }
 
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     n: 1,
-    temperature: 1,
+    temperature: 0.9,
     max_tokens: 500,
     stream: has_onTokenCallback,
     messages: [
-      { role: 'assistant', content: system_setup },
+      ...system_setup,
       ...messages,
     ],
+    ...options,
   }, { responseType: has_onTokenCallback ? 'stream' : null })
   
   if (!has_onTokenCallback) {
@@ -61,63 +70,143 @@ async function ask_the_bot(system_setup, messages, onTokenCallback) {
   }
 }
 
-// const facts = {
-//   _: [
-//     `Volt Europa, Volt Deutschland, Volt Potsdam usw. werden Volt abgekürzt.`,
-//     `Volt selber ist keine Abkürzung und die einzelnen Buchstaben stehen nicht für einzelne Worte.`,
-//     `Der Italiener Andrea Venzon, die Französin Colombe Cahen-Salvador und der Deutsche Damian Boeselager gründeten Volt Europa am 29. März 2017.`,
-//     `In Deutschland wurde Volt als Partei am 3. März 2018 registriert.`,
-//     `Volt ist eine europaweite Partei und Bewegung.`,
-//     `Seit 2019 sitzt Damian Boeselager für Volt Europa im EU-Parlament.`,
-//     `Unter https://www.voltdeutschland.org/mitmachen kannst du dich bei Volt anmelden und mitmachen.`,
-//     `Volt Deutschland ist in 12 Bundesländern aktiv.`,
-//     `Volt Deutschland ist nicht im Bundestag vertreten.`,
-//     `Volt ist in einigen kommunalen Parlamenten in der Regierung beteiligt.`,
-//     `Volt Europa hat zirka 18000 Mitglieder. (Stand 2023-03-02)`,
-//     `Volt Niederlande hat zirka 12000 Mitglieder. (Stand 2023-03-02)`,
-//     `Volt Deutschland hat zirka 4000 Mitglieder. (Stand 2023-03-02)`,
-//     `Volt Brandenburg hat zirka 60 Mitglieder. (Stand 2023-03-02)`,
-//     `Volt Potsdam hat zirka 15 Mitglieder. (Stand 2023-03-02)`,
-//     `Volt existiert in Albania, Austria, Belgium, Bulgaria, Switzerland, Cyprus, Česká republika, Germany, Danmark, Estonia, España, France, Greece, Croatia, Hungary, Ireland, Italia, Lithuania, Luxembourg, Latvia, Malta, Nederland, Poland, Portugal, Romania, Slovenia, Slovensko, Suomi, Sverige, Ukraine, UK, Schottland.`,
-//     `Volt hat über 100 gewählte Vertreter*innen in Europa.`,
-//     `Unter https://www.voltbrandenburg.org/spenden kann man an Volt Brandenburg spenden.`,
-//     `T-Shirts, Sweatshirts, Flaggen und vieles mehr gibt es unter https://merch.voltdeutschland.org/ (der Volt DE Shop)`,
-//     `Volt hat 6,88 Prozent in Darmstadt erreicht.`,
-//     `In Bonn haben 5,07 Prozent für Volt gestimmt. (In Köln 4,98 Prozent)`,
-//     `Volt ist in den nationalen Parlamenten der Niederlande und Bulgarien vertreten.`,
-//     `Der Bundesvorstand von Volt Deutschland besteht aus Rebekka Müller (Vorsitzende), Tim Marton (Vorsitzender), Leo Lüddecke (Schatzmeister), Carina Beckmann (Stv), Martin Finck (Stv), Jennifer Scharpenberg (Stv) und Jens Többen (Stv).`,
-//     `Volt Europa is lead by the Co-Presidents Francesca Romana D'Antuono and Reinier Van Lanschot.`,
-//     `Volt main topics are the 5+1 Challenges: Smart State, Economic Renaissance, Social Equality, Global Balance, Citizen Empowerment and EU Reform.`,
-//     `Volts 5 Challenges are basically the same for every country, but their implementation can be adapted on the national level to take local realities into account. `,
-//     `Der Slogan von Volt lautet "Future. Made in Europe."`,
-//     `Volt liebt Europa, aber möchte die EU reformieren.`,
-//     `Evelyn Steffens und Benjamin Körner sind Landesvorsitzende von Volt Brandenburg. Stellvertreten von Annika Robohm und Guido Richter.`,
-//     `Die City-Leads von Volt Potsdam sind Thomas Rosen und Georg Sichardt.`,
-//     `Potsdam kann unter potsdam@voltdeutschland.org erreicht werden.`,
-//     `Volt Brandenburg hat die Email Adresse: brandenburg@volteuropa.org. Damit können auch Städte in BB erreicht werden.`,
-//     `Potsdam, Cottbus, Lübbenau, Brandenburg an der Havel, usw sind Städte in Brandenburg.`,
-//     `Das nächste Meet&Greet in Potsdam ist am 06.03.2023 um 19 Uhr.`,
-//     `Generell ist jeden Donnerstag um 19 Uhr ein Arbeitstreffen in Potsdam. Dies findet in einer lockern Arbeitsatmosphäre statt.`,
-//     `Am ersten Donnerstag im Monat ist um 29 Uhr Meet&Greet anstatt Arbeitstreffen. Dies findet meisten in einem Café oder einer Bar statt.`,
-//     `Ab und zu veranstalten wir Infostände oder verteilen Flyer.`,
-//     `Unregelmäßig gibt es Team-Events wie Bouldern oder Grillen.`,
-//     `Infos zum Meet&Greet gibt es auf Instagram (@voltpotsdam) und der Webseite (https://www.voltbrandenburg.org/potsdam)`,
-//   ],
-// }
-//
-// function build_facts(categories = []) {
-//   categories.push('_')
-//
-//   let facts_ = []
-//   for (let category of categories) {
-//     if (!facts[category]) continue
-//     facts_.push(...facts[category])
-//   }
-//
-//   return facts_
-// }
+function fact(strings) {
 
-const system_setup = `Du bist ein Fakten-Frage-Antwort-Bot.
+  function first_string(strings) {
+    return (strings || [])
+      .filter(Boolean)
+      .map(string => string.trim())
+      .pop()
+  }
+
+  const tags = (first_string(strings) || '')
+    .split('|')
+    .filter(Boolean)
+
+  return strings => {
+    const text = first_string(strings) || ''
+    return {
+      text,
+      tags,
+    }
+  }
+}
+
+const facts = [
+  fact`Name|Abkürzung``Volt Europa, Volt Deutschland, Volt Potsdam usw. werden Volt abgekürzt.`,
+  fact`Name|Abkürzung``Volt selber ist keine Abkürzung und die einzelnen Buchstaben stehen nicht für einzelne Worte.`,
+  fact`Abkürzung``MG steht für Meet&Greet.`,
+  fact`Europa|Personen``Der Italiener Andrea Venzon, die Französin Colombe Cahen-Salvador und der Deutsche Damian Boeselager gründeten Volt Europa am 29. März 2017.`,
+  fact`Statistiken|Deutschland``In Deutschland wurde Volt als Partei am 3. März 2018 registriert.`,
+  fact`Policy``Volt ist eine europaweite Partei und Bewegung.`,
+  fact`Europa|Personen``Seit 2019 sitzt Damian Boeselager für Volt Europa im EU-Parlament.`,
+  fact`Mitmachen``Unter https://www.voltdeutschland.org/mitmachen kannst du dich bei Volt anmelden und mitmachen.`,
+
+  fact`Statistiken|Europa|Länder``Volt existiert in Albania, Austria, Belgium, Bulgaria, Switzerland, Cyprus, Česká republika, Germany, Danmark, Estonia, España, France, Greece, Croatia, Hungary, Ireland, Italia, Lithuania, Luxembourg, Latvia, Malta, Nederland, Poland, Portugal, Romania, Slovenia, Slovensko, Suomi, Sverige, Ukraine, UK, Schottland.`,
+  fact`Statistiken|Europa|Personen``Volt hat über 100 gewählte Vertreter*innen in Europa.`,
+  fact`Brandenburg|Spenden``Unter https://www.voltbrandenburg.org/spenden kann man an Volt Brandenburg spenden.`,
+  fact`Deutschland|Shop``T-Shirts, Sweatshirts, Flaggen und vieles mehr gibt es unter https://merch.voltdeutschland.org/ (der Volt DE Shop)`,
+
+  fact`Deutschland|Vorsitz|Personen``Der Bundesvorstand von Volt Deutschland besteht aus Rebekka Müller (Vorsitzende), Tim Marton (Vorsitzender), Leo Lüddecke (Schatzmeister), Carina Beckmann (Stv), Martin Finck (Stv), Jennifer Scharpenberg (Stv) und Jens Többen (Stv).`,
+  fact`Europa|Vorsitz|Personen``Volt Europa is lead by the Co-Presidents Francesca Romana D'Antuono and Reinier Van Lanschot.`,
+  fact`Policy``Volt main topics are the 5+1 Challenges: Smart State, Economic Renaissance, Social Equality, Global Balance, Citizen Empowerment and EU Reform.`,
+  fact`Policy``Volts 5 Challenges are basically the same for every country, but their implementation can be adapted on the national level to take local realities into account. `,
+  fact`Policy``Der Slogan von Volt lautet "Future. Made in Europe."`,
+
+  fact`Europa``Volt liebt Europa, aber möchte die EU reformieren.`,
+  fact`Brandenburg|Landesvorsitz|Personen``Evelyn Steffens und Benjamin Körner sind Landesvorsitzende von Volt Brandenburg. Stellvertreten von Annika Robohm und Guido Richter.`,
+  fact`Potsdam|City-Leads|Personen``Die City-Leads von Volt Potsdam sind Thomas Rosen und Georg Sichardt.`,
+  fact`Potsdam|Email``Potsdam kann unter potsdam@voltdeutschland.org erreicht werden.`,
+  fact`Brandenburg|Email``Volt Brandenburg hat die Email Adresse: brandenburg@volteuropa.org. Damit können auch Städte in BB erreicht werden.`,
+  fact`Brandenburg|Potsdam|Cottbus|Lübbenau|Brandenburg an der Havel``Potsdam, Cottbus, Lübbenau, Brandenburg an der Havel, usw sind Städte in Brandenburg.`,
+  fact`Events|Potsdam``Das nächste Meet&Greet in Potsdam ist am 06.03.2023 um 19 Uhr.`,
+  fact`Events|Potsdam``Generell ist jeden Donnerstag um 19 Uhr ein Arbeitstreffen in Potsdam. Dies findet in einer lockern Arbeitsatmosphäre statt.`,
+  fact`Events|Potsdam``Am ersten Donnerstag im Monat ist um 29 Uhr Meet&Greet anstatt Arbeitstreffen. Dies findet meisten in einem Café oder einer Bar statt.`,
+  fact`Events|Potsdam``Ab und zu veranstalten wir Infostände oder verteilen Flyer.`,
+  fact`Events|Potsdam``Unregelmäßig gibt es Team-Events wie Bouldern oder Grillen.`,
+  fact`Events|Socialmedia|Webseite|Potsdam|Brandenburg``Infos zum Meet&Greet gibt es auf Instagram (@voltpotsdam) und der Webseite (https://www.voltbrandenburg.org/potsdam)`,
+
+  fact`Statistiken|Deutschland``Volt Deutschland ist in 12 Bundesländern aktiv.`,
+  fact`Deutschland|Bundestag``Volt Deutschland ist nicht im Bundestag vertreten.`,
+  fact`kommunal|Erfolg|Regierung``Volt ist in einigen kommunalen Parlamenten in der Regierung beteiligt.`,
+  fact`kommunal|Erfolg|Prozent|Darmstadt``Volt hat 6,88 Prozent in Darmstadt erreicht.`,
+  fact`kommunal|Erfolg|Prozent|Bonn``In Bonn haben 5,07 Prozent für Volt gestimmt. (In Köln 4,98 Prozent)`,
+  fact`national|Erfolg|Niederlande|Bulgarien``Volt ist in den nationalen Parlamenten der Niederlande und Bulgarien vertreten.`,
+  fact`Statistiken|Europa``Volt Europa hat zirka 18000 Mitglieder. (Stand 2023-03-02)`,
+  fact`Statistiken|Niederlande``Volt Niederlande hat zirka 12000 Mitglieder. (Stand 2023-03-02)`,
+  fact`Statistiken|Deutschland``Volt Deutschland hat zirka 4000 Mitglieder. (Stand 2023-03-02)`,
+  fact`Statistiken|Brandenburg``Volt Brandenburg hat zirka 60 Mitglieder. (Stand 2023-03-02)`,
+  fact`Statistiken|Potsdam``Volt Potsdam hat zirka 15 Mitglieder. (Stand 2023-03-02)`,
+    
+  fact`City-Lead|Potsdam``City-Leads von Volt Potsdam: Thomas Rosen und Georg Sichardt`,
+]
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+async function get_matching_facts(text) {
+  const max_facts = 20
+
+  if (typeof text !== 'string' || text.length === 0) {
+    // get the N facts with most matching tags
+    const found_facts = shuffle(facts)
+      .slice(0, max_facts)
+      .map(f => `- ${f.text}`)
+      .join('\n')
+    return found_facts
+  }
+
+  const facts_retrival_system_setup = `Wähle fünf Kategorie aus, die am besten helfen die Fragestellung zu beantworten. Verwende strikt nur die möglichen Kategorien. Trenne diese strikt mit Kommas, wie in den möglichen Kategorien vorgegeben.
+
+Mögliche Kategorien: Name, Abkürzung, Europa, Personen, Statistiken, Deutschland, Policy, Mitmachen, Länder, Brandenburg, Spenden, Shop, Vorsitz, Landesvorsitz, Potsdam, City-Leads, Email, Cottbus, Lübbenau, Brandenburg an der Havel, Events, Socialmedia, Webseite, Bundestag, kommunal, Erfolg, Regierung, Prozent, Darmstadt, Bonn, national, Niederlande, Bulgarien, City-Lead
+
+Fragestellung: ${text}`
+
+  const full_text = await ask_the_bot(null, [{ role: 'user', content: facts_retrival_system_setup }], null, {
+    temperature: 0.5,
+  })
+
+  const found_categories = full_text
+    .replace('.', ' ')
+    .split(',')
+    .map(t => t
+      .replace(/\n/g, ' ')
+      .trim()
+    )
+
+  // get the N facts with most matching tags
+  const found_facts = shuffle(facts)
+    .map(fact => {
+      const matching_tags_count = fact.tags.filter(tag => found_categories.includes(tag)).length
+      return { ...fact, matching_tags_count }
+    })
+    .sort((a, b) => b.matching_tags_count - a.matching_tags_count)
+    .slice(0, max_facts)
+    .map(f => `- ${f.text}`)
+    .join('\n')
+
+  return found_facts
+}
+
+async function get_system_setup(text) {
+  const found_facts = await get_matching_facts(text)
+
+  return `Du bist ein Fakten-Frage-Antwort-Bot.
 
 Regeln des Bots:
 - Du bist ein Frage-Antwort-Bot.
@@ -135,45 +224,7 @@ Regeln des Bots:
 - Du musst in der Sprache der Frage antworten. Übersetzte die Fakten oder Verweigerung in die jeweilige Sprache der Frage.
 
 Die gegebenen Fakten:
-- Volt Europa, Volt Deutschland, Volt Potsdam usw. werden Volt abgekürzt.
-- Volt selber ist keine Abkürzung und die einzelnen Buchstaben stehen nicht für einzelne Worte.
-- Der Italiener Andrea Venzon, die Französin Colombe Cahen-Salvador und der Deutsche Damian Boeselager gründeten Volt Europa am 29. März 2017.
-- In Deutschland wurde Volt als Partei am 3. März 2018 registriert.
-- Volt ist eine europaweite Partei und Bewegung.
-- Seit 2019 sitzt Damian Boeselager für Volt Europa im EU-Parlament.
-- Unter https://www.voltdeutschland.org/mitmachen kannst du dich bei Volt anmelden und mitmachen.
-- Volt Deutschland ist in 12 Bundesländern aktiv.
-- Volt Deutschland ist nicht im Bundestag vertreten.
-- Volt ist in einigen kommunalen Parlamenten in der Regierung beteiligt.
-- Volt Europa hat zirka 18000 Mitglieder. (Stand 2023-03-02)
-- Volt Niederlande hat zirka 12000 Mitglieder. (Stand 2023-03-02)
-- Volt Deutschland hat zirka 4000 Mitglieder. (Stand 2023-03-02)
-- Volt Brandenburg hat zirka 60 Mitglieder. (Stand 2023-03-02)
-- Volt Potsdam hat zirka 15 Mitglieder. (Stand 2023-03-02)
-- Volt existiert in Albania, Austria, Belgium, Bulgaria, Switzerland, Cyprus, Česká republika, Germany, Danmark, Estonia, España, France, Greece, Croatia, Hungary, Ireland, Italia, Lithuania, Luxembourg, Latvia, Malta, Nederland, Poland, Portugal, Romania, Slovenia, Slovensko, Suomi, Sverige, Ukraine, UK, Schottland.
-- Volt hat über 100 gewählte Vertreter*innen in Europa.
-- Unter https://www.voltbrandenburg.org/spenden kann man an Volt Brandenburg spenden.
-- T-Shirts, Sweatshirts, Flaggen und vieles mehr gibt es unter https://merch.voltdeutschland.org/ (der Volt DE Shop)
-- Volt hat 6,88 Prozent in Darmstadt erreicht.
-- In Bonn haben 5,07 Prozent für Volt gestimmt. (In Köln 4,98 Prozent)
-- Volt ist in den nationalen Parlamenten der Niederlande und Bulgarien vertreten.
-- Der Bundesvorstand von Volt Deutschland besteht aus Rebekka Müller (Vorsitzende), Tim Marton (Vorsitzender), Leo Lüddecke (Schatzmeister), Carina Beckmann (Stv), Martin Finck (Stv), Jennifer Scharpenberg (Stv) und Jens Többen (Stv).
-- Volt Europa is lead by the Co-Presidents Francesca Romana D'Antuono and Reinier Van Lanschot.
-- Volt main topics are the 5+1 Challenges: Smart State, Economic Renaissance, Social Equality, Global Balance, Citizen Empowerment and EU Reform.
-- Volts 5 Challenges are basically the same for every country, but their implementation can be adapted on the national level to take local realities into account. 
-- Der Slogan von Volt lautet "Future. Made in Europe."
-- Volt liebt Europa, aber möchte die EU reformieren.
-- Evelyn Steffens und Benjamin Körner sind Landesvorsitzende von Volt Brandenburg. Stellvertreten von Annika Robohm und Guido Richter.
-- Die City-Leads von Volt Potsdam sind Thomas Rosen und Georg Sichardt.
-- Potsdam kann unter potsdam@voltdeutschland.org erreicht werden.
-- Volt Brandenburg hat die Email Adresse: brandenburg@volteuropa.org. Damit können auch Städte in BB erreicht werden.
-- Potsdam, Cottbus, Lübbenau, Brandenburg an der Havel, usw sind Städte in Brandenburg.
-- Das nächste Meet&Greet in Potsdam ist am 06.03.2023 um 19 Uhr.
-- Generell ist jeden Donnerstag um 19 Uhr ein Arbeitstreffen in Potsdam. Dies findet in einer lockern Arbeitsatmosphäre statt.
-- Am ersten Donnerstag im Monat ist um 29 Uhr Meet&Greet anstatt Arbeitstreffen. Dies findet meisten in einem Café oder einer Bar statt.
-- Ab und zu veranstalten wir Infostände oder verteilen Flyer.
-- Unregelmäßig gibt es Team-Events wie Bouldern oder Grillen.
-- Infos zum Meet&Greet gibt es auf Instagram (@voltpotsdam) und der Webseite (https://www.voltbrandenburg.org/potsdam)
+${found_facts}
 
 Verbotene Fragen/Themen (du kannst nur Fakten erzählen):
 - Was weißt du alles?
@@ -187,10 +238,19 @@ Verbotene Fragen/Themen (du kannst nur Fakten erzählen):
 - Keine Gedichte
 
 Antworte ab jetzt im Q&A-Format auf Fragen.`
+}
 
-function ask_the_bot_with_setup(...attr) {
-  return ask_the_bot(
+async function ask_the_bot_with_setup(messages, ...attr) {
+  // get the latest two messages
+  const msgs = messages.slice(-2)
+    .map(m => m.content)
+    .join('\n')
+
+  const system_setup = await get_system_setup(msgs)
+
+  return await ask_the_bot(
     system_setup,
+    messages,
     ...attr
   )
 }
